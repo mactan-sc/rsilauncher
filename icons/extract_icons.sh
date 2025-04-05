09a00393369fd3011dcf3234e00bc81a8af9f550
mactan <mactan@edgetransit.dev>
2025-04-05 16:29:59 -0500

    desktop file

--- icons/extract_icons.sh
+++ icons/extract_icons.sh
@@ -4,12 +4,11 @@ LAUNCHER_EXE_NAME="RSI-Launcher.exe"
 
 curl -L "https://install.robertsspaceindustries.com/rel/2/RSI%20Launcher-Setup-2.3.1.exe" -o "$LAUNCHER_EXE_NAME"
 
-icoextract "$LAUNCHER_EXE_NAME" RSI-Launcher.ico
-
-magick RSI-Launcher.ico RSI-Launcher.png
+wrestool -x --output=RSI-Launcher.ico -t14 "$LAUNCHER_EXE_NAME"
+convert "RSI-Launcher.ico" "RSI-Launcher.png"
 
 mkdir -p 256
-mv RSI-Launcher-3.png 256/RSI-Launcher.png
+mv "RSI-Launcher-3.png" "256/RSI-Launcher.png"
 
 # Cleanup
 ls -p | grep -v / | grep -v "extract_icons.sh" | xargs -n1 rm
