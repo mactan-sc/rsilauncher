3803100d45424fa90640eb4a846c7b23b8136cb8
mactan <mactan@edgetransit.dev>
2025-04-17 19:28:53 -0500

    protonpath

--- scripts/constants.sh
+++ scripts/constants.sh
@@ -2,10 +2,7 @@
 export GAMEID="umu-starcitizen"
 export STORE="none"
 export WINEPREFIX="$XDG_DATA_HOME"/prefix
-export PROTONPATH="$XDG_DATA_HOME/proton"
+export PROTONPATH=GE-Latest
 
 export launcher_cfg_path="$XDG_CONFIG_HOME/starcitizen-lug"
 export launcher_cfg="launcher.cfg"
-
-export PROTON_LATEST_URL="https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton9-26/GE-Proton9-26.tar.gz"
-export PROTON_LATEST_SHA512="6589a3d5561948f738f59309c93953f1e74a282f4945077188ee866c517e8a2d8196715f484b8e1097964962f3ec047e22da2012df25e81238a96d25d3285091"
