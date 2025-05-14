871cb9f3060dc94e2461539990127d21ace44401
mactan <mactan@edgetransit.dev>
2025-05-14 16:52:55 -0500

    GE-Proton default constant

--- scripts/constants.sh
+++ scripts/constants.sh
@@ -2,7 +2,7 @@
 export GAMEID="umu-starcitizen"
 export STORE="none"
 export WINEPREFIX="$XDG_DATA_HOME"/prefix
-export PROTONPATH="GE-Latest"
+export PROTONPATH="GE-Proton"
 
 export launcher_cfg_path="$XDG_CONFIG_HOME/starcitizen-lug"
 export launcher_cfg="launcher.cfg"
