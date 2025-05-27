f2456b367fb9c0fe6980bd18934a677777965ea5
mactan <mactan@edgetransit.dev>
2025-05-27 16:32:56 -0500

    maintenance app icon

--- icons/extract_icons.sh
+++ icons/extract_icons.sh
@@ -6,9 +6,11 @@ curl -L "https://install.robertsspaceindustries.com/rel/2/RSI%20Launcher-Setup-2
 
 wrestool -x --output=RSI-Launcher.ico -t14 "$LAUNCHER_EXE_NAME"
 convert "RSI-Launcher.ico" "RSI-Launcher.png"
+convert RSI-Launcher-3.png wrench-256.png -geometry 75x75+175+10 -composite RSI-Launcher-Maintenance.png
 
 mkdir -p 256
 mv "RSI-Launcher-3.png" "256/RSI-Launcher.png"
+mv "RSI-Launcher-Maintenance.png" "256/RSI-Launcher-Maintenance.png"
 
 # Cleanup
 ls -p | grep -v / | grep -v "extract_icons.sh" | xargs -n1 rm
