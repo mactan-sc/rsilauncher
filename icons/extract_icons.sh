3479158974ae394166b4bad297ad09fc2b26cc09
mactan <mactan@edgetransit.dev>
2025-05-31 16:11:52 -0500

    bump rsi launcher version

--- icons/extract_icons.sh
+++ icons/extract_icons.sh
@@ -2,7 +2,7 @@
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 LAUNCHER_EXE_NAME="RSI-Launcher.exe"
 
-curl -L "https://install.robertsspaceindustries.com/rel/2/RSI%20Launcher-Setup-2.3.1.exe" -o "$LAUNCHER_EXE_NAME"
+curl -L "https://install.robertsspaceindustries.com/rel/2/RSI%20Launcher-Setup-2.4.0.exe" -o "$LAUNCHER_EXE_NAME"
 
 wrestool -x --output=RSI-Launcher.ico -t14 "$LAUNCHER_EXE_NAME"
 convert "RSI-Launcher.ico" "RSI-Launcher.png"
@@ -13,4 +13,4 @@ mv "RSI-Launcher-3.png" "256/RSI-Launcher.png"
 mv "RSI-Launcher-Maintenance.png" "256/RSI-Launcher-Maintenance.png"
 
 # Cleanup
-ls -p | grep -v / | grep -v "extract_icons.sh" | xargs -n1 rm
+ls -p | grep -v / | grep -v "extract_icons.sh\|wrench-256.png" | xargs -n1 rm
